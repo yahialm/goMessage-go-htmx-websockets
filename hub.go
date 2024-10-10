4e20e4f999dcdc4353e05840c680369318fc5a56
yahialm <yahia.lamhafad@aiac.ma>
2024-10-10 11:03:36 +0100

    more docs for better comprehension

--- hub.go
+++ hub.go
@@ -63,17 +63,25 @@ func (h *Hub) run() {
 
 func getMessageTemplate(msg *Message) []byte {
 
+	// This function loads the template file (message.html) and parses it.
 	tmpl, err := template.ParseFiles("templates/message.html")
 	if err != nil {
 		log.Fatalf("Template parsing error: %s", err)
 	}
 
+	// This creates a buffer (a temporary in-memory storage area) to hold the rendered template.
+	// Once the placeholders in the template are replaced with the actual message data,
+	// the final HTML will be written to this buffer.
 	var renderedMessage bytes.Buffer
+
+	// This line actually processes the template and replaces 
+	// placeholders (like {{ .UserId }} and {{ .Message }}) with values from the msg struct
 	err = tmpl.Execute(&renderedMessage, msg)
 	if err != nil {
 		log.Fatalf("execution error: %s", err)
 	}
 
+	
 	return renderedMessage.Bytes()
 
 }
